ca230c02fce9bb38f404b21e4dae1d344207e820
jangseyeong <jang.se.yeong000@gmail.com>
2024-05-19 19:43:20 +0900

    feat: 대시보드 감정 리스트 추가

--- src/pages/dashboard/components/EmotionList.tsx
+++ src/pages/dashboard/components/EmotionList.tsx
@@ -1,27 +1,110 @@
 import styled from 'styled-components';
-import { flexColumnCenter } from '@styles/CommonStyles';
+import { divider, flexBetween, flexCenter, flexColumnCenter } from '@styles/CommonStyles';
 
 import type { EmotionAmountTotalType } from '@models/api/dashboard';
-import { calculatePercentages } from '@utils/index';
+import { addCommasToNumber, calculatePercentages } from '@utils/index';
+import { getEmotionIcon, getEmotionText } from '@models/emotion';
 
 type EmotionChartProps = {
   data: EmotionAmountTotalType[];
 };
 
-const EmotionChart = ({ data }: EmotionChartProps) => {
+const EmotionList = ({ data }: EmotionChartProps) => {
+  // 합계 계산
+  const total = data.reduce((acc, value) => acc + value.amount, 0);
+
+  // 각 항목의 비율 계산 후 데이터에 합치기
+  const percentArray = calculatePercentages(data.map((data) => data.amount));
+  const mergedPercentData = data.map((data, i) => {
+    return { ...data, percent: percentArray[i] };
+  });
   return (
     <Container>
-      {/* <div>{JSON.stringify(data)}</div> */}
-      <div>{calculatePercentages(data.map((data) => data.amount)).join('/')}</div>
+      <Total>
+        <span>전체</span>
+        <span>{`${addCommasToNumber(total)}원`}</span>
+      </Total>
+      <Divider />
+      <List>
+        {mergedPercentData
+          .sort((a, b) => b.amount - a.amount)
+          .map((data) => {
+            const EmotionSVG = getEmotionIcon(data.emotion);
+            return (
+              <ListWrapper key={data.emotion}>
+                <EmotionInfo>
+                  <EmotionIcon>
+                    <EmotionSVG />
+                  </EmotionIcon>
+                  <span>{getEmotionText(data.emotion)}</span>
+                  <span className="percent">{`${data.percent}%`}</span>
+                </EmotionInfo>
+                <AmountInfo>{`${addCommasToNumber(data.amount)}원`}</AmountInfo>
+              </ListWrapper>
+            );
+          })}
+      </List>
     </Container>
   );
 };
 
-export default EmotionChart;
+export default EmotionList;
 
 const Container = styled.div`
   ${flexColumnCenter}
+  justify-content: flex-start;
   width: 100%;
   height: 200px;
-  background-color: green;
+  gap: 12px;
+`;
+
+const Divider = styled.div`
+  ${divider}
+  margin: 0;
+`;
+
+const Total = styled.div`
+  ${flexBetween}
+  width: 100%;
+  color: #9f9f9f;
+  font-size: 14px;
+  font-weight: 500;
+  padding: 0 10px 0 10px;
+`;
+
+const List = styled.div`
+  ${flexColumnCenter}
+  width: 100%;
+  gap: 12px;
+  overflow-y: auto;
+  padding: 0 10px 0 10px;
+`;
+
+const ListWrapper = styled.div`
+  ${flexBetween}
+  width: 100%;
+`;
+
+const EmotionInfo = styled.div`
+  ${flexCenter}
+  font-size: 14px;
+  font-weight: 400;
+  color: #333331;
+
+  & > span.percent {
+    color: #9f9f9f;
+    margin-left: 8px;
+  }
+`;
+const AmountInfo = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const EmotionIcon = styled.div`
+  ${flexCenter}
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-right: 12px;
 `;
