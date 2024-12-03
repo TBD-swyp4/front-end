79cc62af996eb520446716f9ce9ff9214143bae2
jangseyeong <jang.se.yeong000@gmail.com>
2024-12-04 00:31:16 +0900

    chore: vite-plugin-pwa 삭제, font preload에 crossorigin 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
-// import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
